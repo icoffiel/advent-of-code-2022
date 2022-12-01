f701efbc5ee46530cff52acc261cff951c3952f2
Iain <iain.coffield@gmail.com>
2022-12-01 08:51:12 -0600

    Day 1 implementation

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,30 @@
 fun main() {
+    fun parseToSums(input: List<String>) = input
+        .joinToString("-")
+        .split("--")
+        .map { elf ->
+            elf
+                .split("-")
+                .sumOf { it.toInt() }
+        }
+        .sorted()
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return parseToSums(input)
+            .takeLast(1)
+            .first()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return parseToSums(input)
+            .takeLast(3)
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
