da8d8b7811e2b4f3415272e7692d78992e530e54
Iain <iain.coffield@gmail.com>
2022-12-03 12:12:42 -0600

    Day 3 - Initial Implementation

---
+++ src/Day03.kt
@@ -0,0 +1,35 @@
+private const val UPPERCASE_ASCII_RESET = 38
+private const val LOWERCASE_ASCII_RESET = 96
+
+fun main() {
+
+    fun Char.toPriority() = when {
+        isUpperCase() -> code - UPPERCASE_ASCII_RESET
+        isLowerCase() -> code - LOWERCASE_ASCII_RESET
+        else -> error("Unexpected condition")
+    }
+
+    fun part1(input: List<String>): Int {
+        return input
+            .map { it.chunked(it.length / 2) }
+            .flatMap { it[0].toList() intersect it[1].toList() }
+            .sumOf { it.toPriority() }
+    }
+
+    fun part2(input: List<String>): Int {
+        return input
+            .chunked(3)
+            .flatMap { it[0].toList() intersect it[1].toList() intersect it[2].toList() }
+            .sumOf { it.toPriority() }
+    }
+
+    val testInput = readInput("Day03_test")
+    check(part1(testInput) == 157)
+    check(part2(testInput) == 70)
+
+    val input = readInput("Day03")
+    println(part1(input))
+    check(part1(input) == 8085)
+    println(part2(input))
+    check(part2(input) == 2515)
+}
